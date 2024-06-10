e340f64444a8728a0e47f481e70d7733faa4a262
Kudamasangomai <Kudam775@gmail.com>
2024-06-10 21:07:16 +0200

    Basic Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use function Laravel\Prompts\password;
+
 class AuthController extends Controller
 {
     public function login(Request $request)
@@ -17,7 +19,7 @@ class AuthController extends Controller
         
         if(!Auth::attempt($validated)){
             return response()->json([
-                'message' => 'No User found'
+                'message' => 'No User Found'
             ],401);
         }
 
@@ -32,6 +34,35 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        
+        $data = $request->validate([
+            'name'=> 'required',
+            'email'=> 'required|email',
+            'password'=> 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+
+
+    public function logout()
+    {
+        Auth::user()->tokens()->delete();
+        return [
+            'message' => 'Succesfully Logged out'
+        ];
     }
 }
